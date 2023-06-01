752211bf3eedaae2c6b68e81af1328cdeed58c11
Cody Scott <mr_bug_man_csb@hotmail.com>
2023-05-31 22:15:18 -0600

    Completed step 4

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -1,16 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.VisualBasic;
 
-namespace Ex6
+namespace Ex6;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args) 
+        //instruction: Give me your birth year...
+        Console.WriteLine("Give me your birth year...");
+        var year = Console.ReadLine();
+        var yearInt = ToInt(year);
+
+        if (yearInt == 0)
         {
-        
+            Console.WriteLine("invalid year. Please re-enter year with a valid int");
+            Main(null);
+        }
+
+        //instruction: Give me your birth month...
+        Console.WriteLine("Give me your birth month...");
+        var month = Console.ReadLine();
+        var monthInt = ToInt(month);
+
+        if (monthInt == 0)
+        {
+            Console.WriteLine("invalid year. Please re-enter year with a valid int");
+            Main(null);
+        }
+
+        //instruction: Give me your birth day...
+        Console.WriteLine("Give me your birth day...");
+        var day = Console.ReadLine();
+        var dayInt = ToInt(day);
+
+        if (dayInt == 0)
+        {
+            Console.WriteLine("invalid year. Please re-enter year with a valid int");
+            Main(null);
+        }
+
+        var birthDate = ToDateTime(yearInt, monthInt, dayInt);
+        ValidateBirthDate(birthDate);
+        Console.WriteLine($"your birthday is {birthDate.ToShortDateString()}!");
+    }
+
+    public static int ToInt(string datePart)
+    {
+        var success = int.TryParse(datePart, out var intDatePart);
+
+        if (success)
+        {
+            return intDatePart;
+        }
+
+        return 0;
+    }
+
+    public static DateTime ToDateTime(int year, int month, int day)
+    {
+        try
+        {
+            var dateTime = new DateTime(year, month, day);
+
+            return dateTime;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine("Please enter a valid date of birth.");
+            Main(null);
+        }
+
+        return new DateTime();
+    }
+
+    public static void ValidateBirthDate(DateTime date)
+    {
+        if (date.Year < 1900)
+        {
+            Console.WriteLine("You are too old asshole! Please enter a valid year");
+            Main(null);
+        }
+
+        if (date.Year > DateTime.Now.Year) 
+        {
+            Console.WriteLine("You aren't alive asshole! Please enter a valid year");
+            Main(null);
         }
     }
 }
